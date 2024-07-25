ef416eb9445d05c934fb415b348bd93c19d96c47
ryan <i@ryani.org>
2024-07-25 21:09:34 +0800

    nvim: add obsidian.nvim

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -88,3 +88,5 @@ vim.o.foldcolumn = '1' -- '0' is not bad
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
+
+vim.opt_local.conceallevel = 2 -- Used by obsidian.nvim
