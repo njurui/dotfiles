dc0fa05c906b9b30386583bd06c9567c288271f5
ryan <i@ryani.org>
2025-01-13 16:49:13 +0800

    nvim: remove left tildes

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -89,5 +89,7 @@ vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to de
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 
+vim.opt.fillchars = {eob = " "}
+
 vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions" -- Used by auto-session
 vim.opt_local.conceallevel = 2 -- Used by obsidian.nvim
