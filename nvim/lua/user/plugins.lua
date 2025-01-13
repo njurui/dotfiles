e528644569406ad8c80352bdb25d612ee8dc2b9e
ryan <i@ryani.org>
2025-01-13 15:44:34 +0800

    nvim: add noice

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -19,10 +19,10 @@ require("lazy").setup({
         priority = 1000,
         config = function()
             require("catppuccin").setup({
-                flavour = "mocha", -- latte, frappe, macchiato, mocha
-                no_italic = false,     -- Force no italic
-                no_bold = false,       -- Force no bold
-                no_underline = false,  -- Force no underline
+                flavour = "mocha",    -- latte, frappe, macchiato, mocha
+                no_italic = false,    -- Force no italic
+                no_bold = false,      -- Force no bold
+                no_underline = false, -- Force no underline
                 transparent_background = true,
                 integrations = {
                     cmp = true,
@@ -212,6 +212,39 @@ require("lazy").setup({
     {
         'stevearc/dressing.nvim',
         opts = {},
+    },
+    -- lazy.nvim
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        config = function()
+            require("noice").setup({
+                lsp = {
+                    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+                    override = {
+                        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                        ["vim.lsp.util.stylize_markdown"] = true,
+                        ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+                    },
+                },
+                -- you can enable a preset for easier configuration
+                presets = {
+                    bottom_search = true, -- use a classic bottom cmdline for search
+                    command_palette = true, -- position the cmdline and popupmenu together
+                    long_message_to_split = true, -- long messages will be sent to a split
+                    inc_rename = false, -- enables an input dialog for inc-rename.nvim
+                    lsp_doc_border = false, -- add a border to hover docs and signature help
+                },
+            })
+        end,
+        dependencies = {
+            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            "MunifTanjim/nui.nvim",
+            -- OPTIONAL:
+            --   `nvim-notify` is only needed, if you want to use the notification view.
+            --   If not available, we use `mini` as the fallback
+            "rcarriga/nvim-notify",
+        }
     },
 
     -- Insert Enhance
