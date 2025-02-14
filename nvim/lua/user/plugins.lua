50b9bc75b1a89a409f62bbada899fab6089f33ab
ryan <i@ryani.org>
2025-02-14 15:39:34 +0800

    nvim: change color group

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -40,9 +40,39 @@ require("lazy").setup({
                     },
                     color_overrides = {
                         mocha = {
-                            base = "#000000",
-                            mantle = "#000000",
-                            crust = "#000000",
+                            rosewater = "#ffc0b9",
+                            flamingo = "#f5aba3",
+                            pink = "#f592d6",
+                            -- mauve = "#c0afff",
+                            red = "#ea746c",
+                            maroon = "#ff8595",
+                            peach = "#fa9a6d",
+                            yellow = "#ffe081",
+                            green = "#99d783", -- include
+                            teal = "#47deb4",
+                            sky = "#00d5ed",
+                            sapphire = "#00dfce",
+                            blue = "#00baee",     -- function
+                            lavender = "#abbff3", -- member
+
+                            text = "#dddddd",
+
+                            subtext1 = "#bbbbbb",
+                            subtext0 = "#aaaaaa",
+                            overlay2 = "#999999",
+                            overlay1 = "#888888",
+                            overlay0 = "#777777",
+                            surface2 = "#666666",
+                            surface1 = "#555555",
+                            surface0 = "#444444",
+
+                            base = "#202020",
+                            mantle = "#222222",
+                            crust = "#333333",
+
+                            -- base = "#000000",
+                            -- mantle = "#000000",
+                            -- crust = "#000000",
                         },
                     },
                 })
