c9b4a44304c5881c0964d3267ddeae84aa1d9286
ryan <i@ryani.org>
2025-11-05 01:39:21 +0800

    zsh: fix history missing commands starting with space

--- zsh/history.zsh
+++ zsh/history.zsh
@@ -10,7 +10,7 @@ unsetopt inc_append_history   # write to the history file immediately, not when 
 setopt extended_history       # record timestamp of command in HISTFILE
 setopt hist_expire_dups_first # delete duplicates first when HISTFILE size exceeds HISTSIZE
 setopt hist_ignore_dups       # don't record an event that was just recorded again
-setopt hist_ignore_space      # ignore commands that start with space
+unsetopt hist_ignore_space    # ignore commands that start with space
 setopt hist_reduce_blanks     # remove superfluous blanks from each command line being added to the history list
 setopt hist_verify            # show command with history expansion to user before running it
 setopt share_history          # share command history data
