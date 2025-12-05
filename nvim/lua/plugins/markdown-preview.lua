bb6124e838f9da609bf5638eeef5f2156ab4b390
ryan <i@ryani.org>
2025-12-06 00:17:42 +0800

    nvim: replace rainbow-delimiter and autopair plugin to blink; use markview

--- nvim/lua/plugins/markdown-preview.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    keys = { { "<leader>mp", "<cmd>MarkdownPreview<cr>", desc = "Markdown preview" } },
-    ft = { "markdown" },
-    build = function()
-        vim.fn["mkdp#util#install"]()
-    end,
-}
