452d639c70b4ba192d510d90dca323489185b399
ryan <i@ryani.org>
2025-12-11 14:45:40 +0800

    setup: use ln -sf and add unzip

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 # install pixi package manager and essential tools
 curl -fsSL https://pixi.sh/install.sh | sh
-~/.pixi/bin/pixi global install zsh tmux==3.5 nvim fzf wget git ripgrep fd-find ffmpeg imagemagick tree-sitter-cli nodejs python
+~/.pixi/bin/pixi global install zsh tmux==3.5 nvim fzf wget git unzip ripgrep fd-find ffmpeg imagemagick tree-sitter-cli nodejs python
 
 # install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain nightly
@@ -12,7 +12,7 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 
 # setup config files
 ~/.pixi/bin/git clone https://github.com/njurui/dotfiles.git ~/.config/dotfiles
-ln -s ~/.config/dotfiles/zsh ~/.config/zsh
-ln -s ~/.config/dotfiles/tmux ~/.config/tmux
-ln -s ~/.config/dotfiles/nvim ~/.config/nvim
-ln -s ~/.config/dotfiles/zsh/.zshrc ~/.zshrc
+ln -sf ~/.config/dotfiles/zsh ~/.config/zsh
+ln -sf ~/.config/dotfiles/tmux ~/.config/tmux
+ln -sf ~/.config/dotfiles/nvim ~/.config/nvim
+ln -sf ~/.config/dotfiles/zsh/.zshrc ~/.zshrc
