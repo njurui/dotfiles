c12da7d9207834c6e03b1acc43b78288e787778a
ryan <i@ryani.org>
2026-01-01 00:45:39 +0800

    nvim: add outline.nvim

---
+++ nvim/lua/plugins/outline.lua
@@ -0,0 +1,18 @@
+return {
+    "hedyhli/outline.nvim",
+    lazy = true,
+    cmd = { "Outline", "OutlineOpen" },
+    keys = { { "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" } },
+    dependencies = {
+        "onsails/lspkind.nvim",
+    },
+    opts = {
+        symbol_folding = {
+            -- Depth past which nodes will be folded by default. Set to false to unfold all on open.
+            autofold_depth = false,
+        },
+        symbols = {
+            icon_source = "lspkind",
+        },
+    },
+}
