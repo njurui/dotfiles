991d2f461040792a6db46bb75d668f8150d12f53
ryan <i@ryani.org>
2026-01-06 14:18:51 +0800

    zsh: update toolchain config

--- zsh/.env.zsh
+++ zsh/.env.zsh
@@ -1,6 +1,6 @@
 # kitty shell integration
 if [[ -n "$KITTY_INSTALLATION_DIR" ]]; then
-    # Must reload shell integration to keep tmux cursor beam
+    # Must reload shell integration to keep tmux/nvim cursor beam
     export KITTY_SHELL_INTEGRATION="enabled"
     autoload -Uz -- "$KITTY_INSTALLATION_DIR"/shell-integration/zsh/kitty-integration
     kitty-integration
@@ -41,27 +41,30 @@ if [[ -d "$KITTY_REMOTE_DIR" ]]; then
     fi
 fi
 
-# Toolchain config
+# Toolchain config in Homebrew
 if [[ -d ${HOMEBREW_PREFIX-} ]]; then
-    # environment for clang/clangd/gcc
+    # environment for clang & clangd & gcc
     export CPATH="$HOMEBREW_PREFIX/include"
 
-    # environment for CMake
+    # environment for Autotools
+    export CPPFLAGS="-I$HOMEBREW_PREFIX/include"
+
+    # environment for CMake & Autotools
     export CFLAGS="-I$HOMEBREW_PREFIX/include"
     export CXXFLAGS="-I$HOMEBREW_PREFIX/include"
     export LDFLAGS="-L$HOMEBREW_PREFIX/lib"
 
     # environment for Cargo
     export RUSTFLAGS="-L$HOMEBREW_PREFIX/lib"
-fi
 
-# Toolchain config (if llvm exists)
-if [[ -n ${HOMEBREW_PREFIX-} && -d "$HOMEBREW_PREFIX/opt/llvm" ]]; then
-    typeset -aU path
-    path=($HOMEBREW_PREFIX/opt/llvm/bin $path)
+    # LLVM config
+    if [[ -d "$HOMEBREW_PREFIX/opt/llvm" ]]; then
+        typeset -aU path
+        path=($HOMEBREW_PREFIX/opt/llvm/bin $path)
 
-    # environment for CMake
-    export CC="$HOMEBREW_PREFIX/opt/llvm/bin/clang"
-    export CXX="$HOMEBREW_PREFIX/opt/llvm/bin/clang++"
-    export LDFLAGS="-L$HOMEBREW_PREFIX/lib -L$HOMEBREW_PREFIX/opt/llvm/lib/c++ -L$HOMEBREW_PREFIX/opt/llvm/lib/unwind -lunwind"
+        # environment for CMake & Autotools
+        export CC="$HOMEBREW_PREFIX/opt/llvm/bin/clang"
+        export CXX="$HOMEBREW_PREFIX/opt/llvm/bin/clang++"
+        export LDFLAGS="$LDFLAGS -L$HOMEBREW_PREFIX/opt/llvm/lib/c++ -L$HOMEBREW_PREFIX/opt/llvm/lib/unwind -lunwind"
+    fi
 fi
