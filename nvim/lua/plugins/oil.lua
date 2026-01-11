01de05fa294902ebcbf334dba267808a0effb6de
ryan <i@ryani.org>
2026-01-11 12:47:11 +0800

    nvim: remove keymap in oil

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,6 +1,11 @@
 return {
     'stevearc/oil.nvim',
-    opts = {},
+    opts = {
+        keymaps = {
+            ["<C-h>"] = false,
+            ["<C-l>"] = false,
+        }
+    },
     dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
     lazy = false,
 }
