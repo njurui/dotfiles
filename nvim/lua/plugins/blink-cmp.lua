fd0a010e34af3d17ec1e28fae6f1b9de345dea50
ryan <i@ryani.org>
2026-01-24 19:59:43 +0800

    nvim: disable auto insert

--- nvim/lua/plugins/blink-cmp.lua
+++ nvim/lua/plugins/blink-cmp.lua
@@ -19,9 +19,10 @@ return {
 		},
 
 		completion = {
-			documentation = {
-				auto_show = true,
-				window = { border = "rounded" },
+			list = {
+				selection = {
+					auto_insert = false,
+				},
 			},
 			menu = {
 				border = "rounded",
@@ -47,16 +48,12 @@ return {
 					},
 				},
 			},
+			documentation = {
+				auto_show = true,
+				window = { border = "rounded" },
+			},
 			ghost_text = {
 				enabled = false,
-				-- Show the ghost text when an item has been selected
-				show_with_selection = true,
-				-- Show the ghost text when no item has been selected, defaulting to the first item
-				show_without_selection = false,
-				-- Show the ghost text when the menu is open
-				show_with_menu = false,
-				-- Show the ghost text when the menu is closed
-				show_without_menu = true,
 			},
 		},
 
