f9d9759c61f787a3b08e76d96ffeda77055457e0
ryan <i@ryani.org>
2026-02-20 15:46:33 +0800

    nvim: update dap keymap

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -109,11 +109,11 @@ return {
 		)
 
 		-- DAP Keymaps
-		vim.keymap.set("n", "<F5>", dap.continue, { desc = "DAP: Continue" })
-		vim.keymap.set("n", "<F9>", dap.toggle_breakpoint, { desc = "DAP: Toggle Breakpoint" })
-		vim.keymap.set("n", "<F10>", dap.step_over, { desc = "DAP: Step Over" })
-		vim.keymap.set("n", "<F11>", dap.step_into, { desc = "DAP: Step Into" })
-		vim.keymap.set("n", "<F12>", dap.step_out, { desc = "DAP: Step Out" })
+		vim.keymap.set("n", "<leader>dc", dap.continue, { desc = "DAP: Continue" })
+		vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "DAP: Toggle Breakpoint" })
+		vim.keymap.set("n", "<leader>do", dap.step_over, { desc = "DAP: Step Over" })
+		vim.keymap.set("n", "<leader>di", dap.step_into, { desc = "DAP: Step Into" })
+		vim.keymap.set("n", "<leader>do", dap.step_out, { desc = "DAP: Step Out" })
 		vim.keymap.set("n", "<leader>dq", dap.terminate, { desc = "DAP: Terminate" })
 
 		-- Setup DAP UI
