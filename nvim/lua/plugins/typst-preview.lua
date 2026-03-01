f1c4a48c9100479ab05773f4228cd6b48a75285f
Rui <rui@Ruis-MacBook-Pro.local>
2026-03-02 06:18:37 +0800

    update typst-preview

--- nvim/lua/plugins/typst-preview.lua
+++ nvim/lua/plugins/typst-preview.lua
@@ -1,16 +1,16 @@
 return {
-	"chomosuke/typst-preview.nvim",
-	ft = "typst",
-	keys = { { "<leader>tp", "<cmd>TypstPreview<cr>", desc = "Typst preview" } },
-	opts = {
-		-- port = 23635,
-		open_cmd = "open -a Firefox %s", -- Use Firefox to avoid lagging
-		dependencies_bin = {
-			["tinymist"] = "tinymist",
-		},
-		get_main_file = function(path_of_buffer)
-			local main = vim.fn.findfile("main.typ", vim.fn.fnamemodify(path_of_buffer, ":p:h") .. ";")
-			return main ~= "" and main or path_of_buffer
-		end,
-	},
+    "chomosuke/typst-preview.nvim",
+    ft = "typst",
+    keys = { { "<leader>tp", "<cmd>TypstPreview<cr>", desc = "Typst preview" } },
+    opts = {
+        -- port = 23635,
+        open_cmd = "firefox %s", -- Use Firefox to avoid lagging
+        dependencies_bin = {
+            ["tinymist"] = "tinymist",
+        },
+        get_main_file = function(path_of_buffer)
+            local main = vim.fn.findfile("main.typ", vim.fn.fnamemodify(path_of_buffer, ":p:h") .. ";")
+            return main ~= "" and main or path_of_buffer
+        end,
+    },
 }
