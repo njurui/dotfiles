b2cf4f78d326e8c546f76cb810b7ec61d475f0a3
Rui Ning <i@rning.site>
2026-04-22 17:23:28 +0800

    nvim: add breadcrumb to statusline

--- nvim/lua/plugins/mini-statusline.lua
+++ nvim/lua/plugins/mini-statusline.lua
@@ -2,46 +2,62 @@ return {
     "nvim-mini/mini.statusline",
     dependencies = {
         "lewis6991/gitsigns.nvim",
+        {
+            "SmiteshP/nvim-navic",
+            opts = {
+                lsp = {
+                    auto_attach = true,
+                },
+                highlight = true,
+                depth_limit = 4,
+                click = false,
+            }
+        }
     },
-    opts = {
-        content = {
-            active = function()
-                local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
-                local git = MiniStatusline.section_git({ trunc_width = 40 })
-                local diff = MiniStatusline.section_diff({ trunc_width = 75 })
-                local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
-                local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
-                local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
-                local location = "%l:%c"
+    config = function()
+        vim.api.nvim_set_hl(0, "MiniStatuslineFilename", {
+            bg = "NONE",
+            ctermbg = "NONE",
+            update = true,
+        })
+        vim.api.nvim_set_hl(0, "MiniStatuslineDevinfo", {
+            bg = "NONE",
+            ctermbg = "NONE",
+            update = true,
+        })
+        vim.api.nvim_set_hl(0, "MiniStatuslineFileinfo", {
+            bg = "NONE",
+            ctermbg = "NONE",
+            update = true,
+        })
+        vim.api.nvim_set_hl(0, "NavicText", { fg = "#dddddd", bg = "NONE" })
+        vim.api.nvim_set_hl(0, "NavicSeparator", { fg = "#6c6c6c", bg = "NONE" })
+        require("mini.statusline").setup({
+            content = {
+                active = function()
+                    local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+                    local git = MiniStatusline.section_git({ trunc_width = 40 })
+                    local diff = MiniStatusline.section_diff({ trunc_width = 75 })
+                    local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+                    local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+                    local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
+                    local location = "%l:%c"
 
-                local filename = vim.fn.expand('%:t')
+                    local breadcrumbs = require("nvim-navic").get_location()
+                    local filename = vim.fn.expand('%:t')
 
-                vim.api.nvim_set_hl(0, "MiniStatuslineFilename", {
-                    bg = "NONE",
-                    ctermbg = "NONE",
-                    update = true,
-                })
-                vim.api.nvim_set_hl(0, "MiniStatuslineDevinfo", {
-                    bg = "NONE",
-                    ctermbg = "NONE",
-                    update = true,
-                })
-                vim.api.nvim_set_hl(0, "MiniStatuslineFileinfo", {
-                    bg = "NONE",
-                    ctermbg = "NONE",
-                    update = true,
-                })
-
-                return MiniStatusline.combine_groups({
-                    { hl = mode_hl,                 strings = { mode:upper() } },
-                    { hl = 'MiniStatuslineDevinfo', strings = { git, diff, diagnostics } },
-                    '%<',
-                    { hl = 'MiniStatuslineFilename', strings = { filename } },
-                    '%=',
-                    { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
-                    { hl = mode_hl,                  strings = { search, location } },
-                })
-            end,
-        },
-    }
+                    return MiniStatusline.combine_groups({
+                        { hl = mode_hl,                 strings = { mode:upper() } },
+                        { hl = 'MiniStatuslineDevinfo', strings = { git, diff, diagnostics } },
+                        '%<',
+                        { hl = 'MiniStatuslineFilename', strings = { filename, breadcrumbs } },
+                        '%=',
+                        { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+                        { hl = mode_hl,                  strings = { search, location } },
+                    })
+                end,
+            },
+        }
+        )
+    end,
 }
