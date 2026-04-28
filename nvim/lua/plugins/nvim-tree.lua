16ad01fe08240d43e7c7205a142997a33a27849d
Rui Ning <i@rning.site>
2026-04-28 18:04:31 +0800

    nvim: always reveal focused file in nvim-tree

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -3,5 +3,11 @@ return {
     keys = { { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "Toggle NvimTree" } },
     opts = {
         filters = { custom = { "^.git$" }, },
+        update_focused_file = {
+            enable = true,
+            update_root = {
+                enable = true,
+            },
+        },
     },
 }
