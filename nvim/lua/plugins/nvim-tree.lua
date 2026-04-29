691260f6e0a93c84b4050a887ed8e834c2a1c1a2
Rui Ning <i@rning.site>
2026-04-29 15:40:14 +0800

    nvim: add diagnostic to nvim-tree

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,13 +1,54 @@
 return {
     "nvim-tree/nvim-tree.lua",
     keys = { { "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "Toggle NvimTree" } },
-    opts = {
-        filters = { custom = { "^.git$" }, },
-        update_focused_file = {
-            enable = true,
-            update_root = {
-                enable = true,
+    config = function()
+        vim.api.nvim_set_hl(0, "NvimTreeDiagnosticErrorFileHL", { link = "DiagnosticError" })
+        vim.api.nvim_set_hl(0, "NvimTreeDiagnosticWarnFileHL", { link = "DiagnosticWarn" })
+        vim.api.nvim_set_hl(0, "NvimTreeDiagnosticInfoFileHL", { link = "DiagnosticInfo" })
+        vim.api.nvim_set_hl(0, "NvimTreeDiagnosticHintFileHL", { link = "DiagnosticHint" })
+
+        require("nvim-tree").setup({
+            view = {
+                signcolumn = "no",
             },
-        },
-    },
+            renderer = {
+                highlight_diagnostics = "name",
+                icons = {
+                    git_placement = "right_align",
+                    show = {
+                        diagnostics = false,
+                    },
+                    glyphs = {
+                        git = {
+                            unstaged = "M",  -- modified
+                            staged = "S",    -- staged
+                            unmerged = "!",  -- conflict
+                            renamed = "R",   -- renamed
+                            untracked = "U", -- untracked
+                            deleted = "D",   -- deleted
+                            ignored = "",
+                        },
+                    },
+                },
+            },
+            update_focused_file = {
+                enable = true,
+                update_root = {
+                    enable = true,
+                },
+            },
+            diagnostics = {
+                enable = true,
+                show_on_dirs = true,
+                show_on_open_dirs = true,
+                icons = {
+                    hint = "󰌶 ",
+                    info = " ",
+                    warning = "󰀪 ",
+                    error = "󰅚 ",
+                },
+            },
+            filters = { custom = { "^.git$" }, },
+        })
+    end,
 }
